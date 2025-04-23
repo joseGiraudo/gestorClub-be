116677778a55776c90d467704b409a57f7f5521f
joseGiraudo <josegiraudo7@gmail.com>
2025-04-23 10:36:00 -0300

    project creation

---
+++ gestorClub-api/src/main/java/pps/gestorClub_api/GestorClubApiApplication.java
@@ -0,0 +1,13 @@
+package pps.gestorClub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorClubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorClubApiApplication.class, args);
+	}
+
+}
