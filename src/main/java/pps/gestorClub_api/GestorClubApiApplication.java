ad0706d9d6a752362049d1a558b69b3ad2b1fcd0
joseGiraudo <josegiraudo7@gmail.com>
2025-06-18 18:09:09 -0300

    enable jpa auditing, members created date

--- src/main/java/pps/gestorClub_api/GestorClubApiApplication.java
+++ src/main/java/pps/gestorClub_api/GestorClubApiApplication.java
@@ -2,8 +2,10 @@ package pps.gestorClub_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GestorClubApiApplication {
 
 	public static void main(String[] args) {
